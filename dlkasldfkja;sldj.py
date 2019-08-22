95c429e420dde64b713b3cf2095a7411dbf77fa2
Nathan Jones <npjones2@ncsu.edu>
2019-08-22 14:04:46 -0400

    Create dlkasldfkja;sldj.py

---
+++ dlkasldfkja;sldj.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 13:41:15 2019
+
+@author: nathanjones
+"""
+
+print('Gekko World')
+print('This is a great file')
+print('Meg is super awesome')
+
+I need to make changes...
+
+DEEEEEEMMMMOOOOOO
